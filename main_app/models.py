1174fdf6f1b99f6ce5c4de42e26d2f04face6709
dmcclendon56 <mcclendon.darrian@gmail.com>
2022-05-30 11:06:40 -0500

    adding routes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class dogs (models.Model):
+
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    type = models.TextField(max_length=500)
+    AKC = models.BooleanField(default=False)
+    CKC = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+# from django.views.generic.edit import CreateView
+# from django.views.generic import DetailView
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"       
+
+
+
