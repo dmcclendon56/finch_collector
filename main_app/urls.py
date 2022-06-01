88cee751a500d6580a74fba2a69cca9d9625ed0b
dmcclendon56 <mcclendon.darrian@gmail.com>
2022-06-01 14:25:51 -0500

    almost to full crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
     path('doglist/', views.DogList.as_view(), name="dog_list"),
+    path('doglist/new', views.DogCreate.as_view(), name="dog_create"),
+    path('doglist/<int:pk>/', views.DogDetail.as_view(), name="dog_detail"),
+    path('doglist/<int:pk>/update', views.DogUpdate.as_view(), name="dog_update"),
+    path('doglist/<int:pk>/delete', views.DogDelete.as_view(), name="dog_delete"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from .models import dogs
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
+# after our other imports 
+from django.views.generic import DetailView
+from django.urls import reverse
 
 # Create your views here.
 class Home(TemplateView):
@@ -17,5 +22,48 @@ class DogList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["dogs"] = Dogs.objects.all() 
+        context["dogs"] = dogs.objects.all() 
         return context
+
+class DogCreate(CreateView):
+    model = dogs
+    fields = ['breed', 'img', 'description', 'AKC/CKC']
+    template_name = "dog_create.html"
+    success_url = "/dogs/"   
+
+    def get_success_url(self):
+        return reverse('dog_detail', kwargs={'pk': self.object.pk})     
+
+
+class DogCreate(View):
+
+    def post(self, request, pk):
+        breed = request.POST.get("breed")
+        description = request.POST.get("description")
+        doggo = dogs.objects.get(pk=pk)
+        dogs.objects.create(breed=breed, description=description, doggo = doggo)
+        return redirect('dog_detail', pk=pk)
+
+class DogDetail(DetailView):
+    model = dogs
+    template_name = "dog_detail.html"   
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["breeds"] = dogs.objects.all()
+        return context 
+
+
+class DogUpdate(UpdateView):
+    model = dogs
+    fields = ['breed', 'img', 'description', 'AKC/CKC']
+    template_name = "dog_create.html"
+    
+
+    def get_success_url(self):
+        return reverse('dog_detail', kwargs={'pk': self.object.pk})
+
+class dogDelete(DeleteView):
+    model = dogs
+    template_name = "dog_delete.html"
+    success_url = "/dog/"    
